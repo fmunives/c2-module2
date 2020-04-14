c4c2376d5a0c14e21151a4044283b9cb68684272
Freddy Munive <munivef@yahoo.com>
2020-04-14 11:11:57 -0500

    adding rate and inputs for information

--- week-1/day-2/compound.rb
+++ week-1/day-2/compound.rb
@@ -1,11 +1,21 @@
-def compound_interest(amount, months)
-  interest = 0.01
+def compound_interest
+
+  print "Enter your money: "
+  amount = gets.chomp.to_i 
+  print "Enter the months: "
+  months = gets.chomp.to_i
+  print "Enter the rate: "
+  rate = gets.chomp.to_f
+
+  rate = rate / 100
   i = 1
   while i <= months
-    amount = amount*(interest+1)
-    i = i + 1
+    amount = amount*(rate+1)
+    i += 1
   end
   amount.round(2)
+
 end
 
-puts compound_interest(1000,3)
+new_amount = compound_interest
+puts "Your new money is S/.#{new_amount}"
