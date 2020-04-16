52474959b9300e7d37c6e71137b0e12cb2101160
Freddy Munive <munivef@yahoo.com>
2020-04-16 16:35:49 -0500

    solution for sum-class

---
+++ week-1/day-4/sum-class.rb
@@ -0,0 +1,16 @@
+class MyArray
+  attr_reader :array
+
+  def initialize(array)
+    @array = array
+  end
+
+  def sum(initial_value = 0)
+    sum = 0
+    @array.each { |x| sum+= x }
+    sum += initial_value
+  end
+end
+
+my_array = MyArray.new([1,2,3])
+puts my_array.sum()
