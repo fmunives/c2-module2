cc487c78aab0228f0a73d9282a74ec47d6245a55
Freddy Munive <munivef@yahoo.com>
2020-04-20 12:01:14 -0500

    refactoring with keywords

--- week-1/day-1/two-fer.rb
+++ week-1/day-1/two-fer.rb
@@ -1,7 +1,18 @@
-def two_fer
-    print "who?"
-    name = gets.chomp.capitalize!
-    !name ? name = "you" : nil
+def two_fer(name:)
+    name = "you" if !name
     "One for #{name}, one for me"
 end
-puts two_fer
+
+print "who?"
+your_name = gets.chomp.capitalize!
+puts two_fer(name:your_name) #refactoring with keywords. 
+
+# old version
+
+# def two_fer
+#     print "who?"
+#     name = gets.chomp.capitalize!
+#     !name ? name = "you" : nil
+#     "One for #{name}, one for me"
+# end
+# puts two_fer
