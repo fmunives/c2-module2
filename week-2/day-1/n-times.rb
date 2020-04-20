45b46fd1f41a1c23c4418f769fc9bb41b89666d8
Freddy Munive <munivef@yahoo.com>
2020-04-20 10:39:37 -0500

    cleaning original branch

--- week-2/day-1/n-times.rb
+++
@@ -1,9 +0,0 @@
-def put_n(n, message)
-  if n.positive? and n.is_a? Integer
-  n.times do puts "#{message} for #{n} times" end
-  else
-    raise ArgumentError.new("Only permitted a positive and interger numbers")
-  end
-end
-
-put_n(-1, "hola que tal")
