b12c130459ecc8c422a1279e8a6727892cfb10c1
Freddy Munive <munivef@yahoo.com>
2020-04-24 09:26:29 -0500

    solution for read_csv

--- week-2/day-4/read_csv.rb
+++ week-2/day-4/read_csv.rb
@@ -1,6 +1,11 @@
-file = File.read("file.csv") do |row| puts row.readlines end 
-# def read_csv(file)
-# end
+require 'csv'
 
+students = []
 
-# read_csv(file)
+CSV.foreach("grades.csv", headers:true) { |row| students << row.to_hash }
+
+top_three = students.sort_by { |top| top["Final"] }.reverse
+
+for i in 0..2 do
+  puts "#{i+1} - #{top_three[i]["First name"]} with a final grade of #{top_three[i]["Final"].to_i}"
+end
