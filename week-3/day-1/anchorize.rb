35c38161845b4cfdb2bc9e57d5881db9fde10db3
Freddy Munive <munivef@yahoo.com>
2020-04-27 20:04:14 -0500

    removing unnecessary regular expresion in the last part

--- week-3/day-1/anchorize.rb
+++ week-3/day-1/anchorize.rb
@@ -1,7 +1,7 @@
 
 def anchorize_me(text)
   begin
-    valid_url = text.match(/(\w+) (?<url>(http|https|ftp|ftps|file|smb):\/\/(\w+\.\w+)) (\S)/i)
+    valid_url = text.match(/(\w+) (?<url>(http|https|ftp|ftps|file|smb):\/\/(\w+\.\w+))/i)
     puts "hello <a href='#{valid_url[:url]}'>#{valid_url[:url]} !"
   rescue Exception => e
     puts "You need to match with a correct global url such as http|https|ftp|ftps|file|smb, error: #{e}"
