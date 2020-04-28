1a91eb6cef26870afd3bdda8c53872a3e77981d6
Freddy Munive <munivef@yahoo.com>
2020-04-27 20:00:14 -0500

    solution for anchorize-me

---
+++ week-3/day-1/anchorize.rb
@@ -0,0 +1,11 @@
+
+def anchorize_me(text)
+  begin
+    valid_url = text.match(/(\w+) (?<url>(http|https|ftp|ftps|file|smb):\/\/(\w+\.\w+)) (\S)/i)
+    puts "hello <a href='#{valid_url[:url]}'>#{valid_url[:url]} !"
+  rescue Exception => e
+    puts "You need to match with a correct global url such as http|https|ftp|ftps|file|smb, error: #{e}"
+  end
+end
+
+anchorize_me('hello smb://world.com !')
